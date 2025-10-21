f50b2e145153b77f29b9174964f716eae8960e46
Li Blissz <liblissz3@gmail.com>
2025-10-20 23:58:07 -0700

    nfn

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const app = express();
 
 app.use(express.json())
 app.use(cors())
+app.options('*', cors());
 app.use("/api/auth", change)
 app.use("/delete", deleting)
 app.use("/api/confirm", confirm)
